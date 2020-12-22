61e5c1abfc210f74181916fcedb1e14df9f467e4
Sebas <sebas.warsaw@gmail.com>
2020-12-22 07:47:08 -0300

    svg performance

--- src/components/Neon.js
+++ src/components/Neon.js
@@ -5,7 +5,9 @@ import anime from 'animejs';
 
 function Neon() {
   React.useEffect(() => {
-    const pathEls = document.querySelectorAll('.neon-wrapper .neon svg path');
+    const pathEls = document.querySelectorAll(
+      '.neon-wrapper .neon svg path:last-child',
+    );
 
     for (let i = 0; i < pathEls.length; i++) {
       const pathEl = pathEls[i];
@@ -22,20 +24,6 @@ function Neon() {
         autoplay: true,
       });
     }
-
-    // const letterEls = document.querySelectorAll('.letter');
-    // for (let i = 0; i < letterEls.length; i++) {
-    //   const letterEl = letterEls[i];
-    //   const offset = anime.setDashoffset(letterEl);
-    //   letterEl.setAttribute('stroke-dashoffset', offset);
-    //   anime({
-    //     targets: letterEl,
-    //     duration: anime.random(1000, 2000),
-    //     delay: anime.random(0, 600),
-    //     opacity: [{ value: 0, duration: anime.random(500, 800) }],
-    //     loop: true,
-    //   });
-    // }
   }, []);
   return (
     <div className="neon-wrapper w-full relative">
@@ -47,11 +35,30 @@ function Neon() {
           viewBox="0 0 400 400"
           xmlns="http://www.w3.org/2000/svg"
         >
-          {/* <path
+          {/* <linearGradient id="gradient">
+            <stop className="main-stop" offset="0%" />
+            <stop className="alt-stop" offset="100%" />
+          </linearGradient> */}
+          <defs>
+            <filter id="circlefilter">
+              <feGaussianBlur
+                in="SourceAlpha"
+                stdDeviation="30"
+                result="desenfoque"
+              />
+              <feFlood floodColor="#2df5ff" />
+              <feComposite in2="desenfoque" operator="in" />
+              <feOffset dx="0" dy="0" />
+              <feMerge>
+                <feMergeNode />
+                <feMergeNode in="SourceGraphic" />
+              </feMerge>
+            </filter>
+          </defs>
+          <path
             className="st0 letter"
-            id="N"
             d="M 120 120 A 50 50 0 1 1 280 280 A 50 50 0 1 1 115 126 L 133 143 A 50 50 0 1 0 262 261 A 50 50 0 1 0 139 139 Z"
-          /> */}
+          />
           <path
             className="st1"
             d="M 120 120 A 50 50 0 1 1 280 280 A 50 50 0 1 1 115 126 L 133 143 A 50 50 0 1 0 262 261 A 50 50 0 1 0 139 139 Z"
@@ -60,18 +67,18 @@ function Neon() {
             className="st2"
             d="M 20 20 L 380 20 L 380 380 L 20 380 L 20 60 L 60 60 L 60 340 L 340 340 L 340 60 L 20 60 Z"
           /> */}
-          <path
+          {/* <path
             className="st3"
             d="M 20 20 L 380 20 L 380 380 L 20 380 L 20 60 L 60 60 L 60 340 L 340 340 L 340 60 L 20 60 Z"
           />
-          {/* <path
+          <path
             className="st4"
             d="M 20 20 L 380 20 L 380 380 L 20 380 L 20 60 L 60 60 L 60 340 L 340 340 L 340 60 L 20 60 Z"
-          /> */}
+          />
           <path
             className="st5"
             d="M 20 20 L 380 20 L 380 380 L 20 380 L 20 60 L 60 60 L 60 340 L 340 340 L 340 60 L 20 60 Z"
-          />
+          /> */}
         </svg>
       </div>
     </div>
