e9972defd5cf332e51501e490771931f7700a75d
Sebs <sebastian.fantini@livestories.com>
2025-02-28 12:07:41 -0300

    fix windows build issue and webpack line break lint

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
